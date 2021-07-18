f11010b9ca4ca19007d1e737e65f224c0027f411
leonardcad02 <leonardcad02@gmail.com>
2021-07-17 20:47:16 -0500

    Load Front to Angular

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  public formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      name: ['Cristian', Validators.required],
+      functionEmployee: ['Development', Validators.required]  
+    });
+   }
+  
+  ngOnInit(): void {
+    
+  }
+  
+
+}
